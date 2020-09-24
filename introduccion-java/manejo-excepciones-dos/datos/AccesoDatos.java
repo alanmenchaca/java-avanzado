f4846f9d2a0aca37213b8fc1638b3e9fadd1e201
Fco. Alan Menchaca M <alanmenchaca22@outlook.com>
2020-09-24 02:56:28 -0500

    git add interface AccesoDatos

---
+++ introduccion-java/manejo-excepciones-dos/datos/AccesoDatos.java
@@ -0,0 +1,15 @@
+package datos;
+
+import excepciones.AccesoDatosEx;
+
+public interface AccesoDatos {
+
+    // Métodos que pueden arrojar Excepciones de tipo subclase es decir;
+    // Escritura de datos o lectura de datos Exception.
+    void insertar() throws AccesoDatosEx;
+
+    // Utilizamos una excepcion de la clase padre en ambos métodos para generalizar el codigo.
+    void listar() throws AccesoDatosEx;
+
+    void simularError(boolean SimularError);
+}
